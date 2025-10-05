1db712860e0031ddddebf77677c63492d927763b
starpetscoding <heartsuluca@gmail.com>
2025-10-05 06:10:54 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -8,4 +8,7 @@ local data = {
               "\nJobId: " .. game.JobId
 }
 
-HttpService:PostAsync("https://discord.com/api/webhooks/1424336790646423585/PHTwg1bVXf3vA_9kDLh1dy-4eZvK0yTKSjf5IRzXelAvXGs36DptM_9LycjoiiP7tg-t", HttpService:JSONEncode(data))
+HttpService:PostAsync(
+    "https://discord.com/api/webhooks/1424336790646423585/PHTwg1bVXf3vA_9kDLh1dy-4eZvK0yTKSjf5IRzXelAvXGs36DptM_9LycjoiiP7tg-t",
+    HttpService:JSONEncode(data)
+)
