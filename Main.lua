3b34fa4fea9d0bc513737cc84b805f3955152e20
starpetscoding <heartsuluca@gmail.com>
2025-10-05 06:07:28 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,11 @@
+local HttpService = game:GetService("HttpService")
+local player = game.Players.LocalPlayer
 
+local data = {
+    content = "**Script Executed!**\nUsername: " .. player.Name ..
+              "\nUserId: " .. player.UserId ..
+              "\nPlaceId: " .. game.PlaceId ..
+              "\nJobId: " .. game.JobId
+}
+
+HttpService:PostAsync("https://discord.com/api/webhooks/1424336790646423585/PHTwg1bVXf3vA_9kDLh1dy-4eZvK0yTKSjf5IRzXelAvXGs36DptM_9LycjoiiP7tg-t", HttpService:JSONEncode(data))
